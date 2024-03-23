d311c573ab8caaf0f922f6fca671d35791425722
jgoyk <joshua@goykhman.com>
2024-03-23 13:16:32 -0400

    Created get, post, and get all routes

--- backend/index.js
+++ backend/index.js
@@ -2,16 +2,54 @@ import mongodb from "mongodb"
 import dotenv from "dotenv"
 import mongoose from "mongoose"
 import express from "express";
+import { User } from "./models/userModel.js";
+
 dotenv.config()
 
 const app = express();
+
+app.use(express.json())
+
 const port = process.env.PORT || 8000
 
 app.get('/', (req,res) => {
     console.log(req);
-    return response.status(234).send("Server Running")
+    return res.status(234).send("Server Running")
 });
 
+app.get('/users', async (req,res) => {
+    try {
+        const users = await User.find({})
+        return res.status(201).json({
+            count: users.length,
+            data: users
+        })
+    } catch(error) {
+        console.log(error.message)
+        res.status(500).send({ message: error.message })
+    }
+})
+
+app.post('/user', async(req,res) => {
+    try{
+        if(
+            !req.body.username 
+        ) {
+            return res.status(400).send({
+                message: "send username",
+            });
+        }
+        const newUser = {
+            username: req.body.username
+        }
+
+        const user = await User.create(newUser)
+        return res.status(201).send(user)
+    } catch (error) {
+        console.log(error.message)
+        res.status(500).send({ message: error.message })
+    }
+})
 
 
 mongoose
